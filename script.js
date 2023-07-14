b5d0f6ac945765f78bfd559a2f635368742bc509
oscar vazquez <solopordiversion2@outlook.com>
2023-07-13 23:43:58 -0700

    se agrego box shadow a imagenes de productos

---
+++ script.js
@@ -0,0 +1,14 @@
+function inicioDeSesion( ){
+    let correo = "text@outlook.com";
+    let contrasenha = "prueba";
+   
+    if ( login__container__input.value = correo && login__container__input.value = "prueba"){
+        alert("acceso autorizado")
+    }
+    else {
+        alert("datos equivocados , intenta de nuevo")
+    }
+
+}
+
+onclick
---
+++ servicios/productos-servicios.js
@@ -0,0 +1,96 @@
+{
+    "nombre": "starwarsProduct"
+    "imagenUrl": "C:\Users\DELL\Desktop\challenge e-commerce\assets\consola (3).png"
+    "precio": "85"
+    "id":1
+}
+{
+    "nombre": "starwarsProduct"
+    "imagenUrl": "C:\Users\DELL\Desktop\challenge e-commerce\assets\consola (3).png"
+    "precio": "85"
+    "id":2
+}
+{
+    "nombre": "starwarsProduct"
+    "imagenUrl": "C:\Users\DELL\Desktop\challenge e-commerce\assets\consola (3).png"
+    "precio": "85"
+    "id":3
+}
+{
+    "nombre": "starwarsProduct"
+    "imagenUrl": "C:\Users\DELL\Desktop\challenge e-commerce\assets\consola (3).png"
+    "precio": "85"
+    "id":4
+}
+{
+    "nombre": "starwarsProduct"
+    "imagenUrl": "C:\Users\DELL\Desktop\challenge e-commerce\assets\consola (3).png"
+    "precio": "85"
+    "id":5
+}
+{
+    "nombre": "starwarsProduct"
+    "imagenUrl": "C:\Users\DELL\Desktop\challenge e-commerce\assets\consola (3).png"
+    "precio": "85"
+    "id":6
+}
+{
+    "nombre": "starwarsProduct"
+    "imagenUrl": "C:\Users\DELL\Desktop\challenge e-commerce\assets\consola (3).png"
+    "precio": "85"
+    "id":7
+}
+{
+    "nombre": "starwarsProduct"
+    "imagenUrl": "C:\Users\DELL\Desktop\challenge e-commerce\assets\consola (3).png"
+    "precio": "85"
+    "id":8
+}
+{
+    "nombre": "starwarsProduct"
+    "imagenUrl": "C:\Users\DELL\Desktop\challenge e-commerce\assets\consola (3).png"
+    "precio": "85"
+    "id":9
+}
+{
+    "nombre": "starwarsProduct"
+    "imagenUrl": "C:\Users\DELL\Desktop\challenge e-commerce\assets\consola (3).png"
+    "precio": "85"
+    "id":10
+}
+{
+    "nombre": "starwarsProduct"
+    "imagenUrl": "C:\Users\DELL\Desktop\challenge e-commerce\assets\consola (3).png"
+    "precio": "85"
+    "id":11
+}
+{
+    "nombre": "starwarsProduct"
+    "imagenUrl": "C:\Users\DELL\Desktop\challenge e-commerce\assets\consola (3).png"
+    "precio": "85"
+    "id":12
+}
+{
+    "nombre": "starwarsProduct"
+    "imagenUrl": "C:\Users\DELL\Desktop\challenge e-commerce\assets\consola (3).png"
+    "precio": "85"
+    "id":13
+}
+{
+    "nombre": "starwarsProduct"
+    "imagenUrl": "C:\Users\DELL\Desktop\challenge e-commerce\assets\consola (3).png"
+    "precio": "85"
+    "id":14
+}
+{
+    "nombre": "starwarsProduct"
+    "imagenUrl": "C:\Users\DELL\Desktop\challenge e-commerce\assets\consola (3).png"
+    "precio": "85"
+    "id":15
+}
+{
+    "nombre": "starwarsProduct"
+    "imagenUrl": "C:\Users\DELL\Desktop\challenge e-commerce\assets\consola (3).png"
+    "precio": "85"
+    "id":16
+}
